ae631a6a31a8b650cbe72b16e4421895d9b9d08f
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2025-09-09 22:37:18 -0700

    Generated Main Class with "Hello World" dummy text

---
+++ chavvi_calc/src/main/java/emily/App.java
@@ -0,0 +1,13 @@
+package emily;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Why does this hate me" );
+    }
+}
