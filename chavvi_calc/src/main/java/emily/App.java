d076e4befc8f463cab76b9d5af9acf2b9e17e3b7
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2025-09-15 12:38:07 -0700

    First attempt to commit changes to a new branch, includes dummy method for testing

--- chavvi_calc/src/main/java/emily/App.java
+++ chavvi_calc/src/main/java/emily/App.java
@@ -10,25 +10,23 @@ import java.lang.Math;
  * Date: 9/25/25
  * Project Name: ChavviCalc App
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         // initialize numerical variables and scanner object
-        // A and B are the two numbers to be calculated, result is the outcome
+        // A and B are the two numbers to be calculated
         // will need to round to 3 sig figs
         double A = 0.000;
         double B = 0.000;
         Scanner input = new Scanner(System.in);
         String choice = "";
 
-         // get user input
+        // get user input
         while (!choice.equals("q")) {
             printMenu(A, B);
             System.out.print("Enter a command: ");
             choice = input.nextLine();
 
-        // switch between menu options to select relevant code
+            // switch between menu options to select relevant code
             switch (choice) {
                 case "a" -> {
                     A = getA(input, A);
@@ -53,21 +51,23 @@ public class App
                 case "q" -> System.out.println("System terminated.");
                 default -> System.out.println("Invalid entry. Please try again.");
             }
-        } 
+        }
 
-         input.close(); }
+        String test = "Something is wrong";
+        System.out.println(branchTest(test));
 
+        input.close();
+    }
 
-
-        // print display menu
-        public static String printMenu (double A, double B) {
-            String menuTitle = """
+    // print display menu
+    public static String printMenu(double A, double B) {
+        String menuTitle = """
                 --------------------------------------
                 Chavvi Calc
                 --------------------------------------
                 """;
-            String menuVariables = "A = " + String.format("%.3f", A) + "\t B = " + String.format("%.3f", B);
-            String menuCommands = """
+        String menuVariables = "A = " + String.format("%.3f", A) + "\t B = " + String.format("%.3f", B);
+        String menuCommands = """
                 --------------------------------------
                 a \t Enter a value for A
                 b \t Enter a value for B
@@ -81,100 +81,101 @@ public class App
                 """;
         String menu = (menuTitle + menuVariables + "\n" + menuCommands);
         System.out.println(menu);
-            return menu;
-        }
-       
-        public static double getA(Scanner input, double A) {
-            System.out.print("Enter a value for A: ");
-                    A = input.nextDouble();
-                    input.nextLine();
-                    return A;
-        }
-    
-        public static double getB(Scanner input, double B) {
-            System.out.print("Enter a value for B: ");
-                    B = input.nextDouble();
-                    input.nextLine();
-                    return B;
-        }
+        return menu;
+    }
 
-        public static double getSum(double A, double B) {
-            A = A + B;
+    public static double getA(Scanner input, double A) {
+        System.out.print("Enter a value for A: ");
+        A = input.nextDouble();
+        input.nextLine();
+        return A;
+    }
+
+    public static double getB(Scanner input, double B) {
+        System.out.print("Enter a value for B: ");
+        B = input.nextDouble();
+        input.nextLine();
+        return B;
+    }
+
+    public static double getSum(double A, double B) {
+        A = A + B;
+        return A;
+    }
+
+    public static double getDifference(double A, double B) {
+        A = A - B;
+        return A;
+    }
+
+    public static double getProduct(double A, double B) {
+        A = A * B;
+        return A;
+    }
+
+    public static double getQuotient(double A, double B) {
+        if (B == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+            return A;
+        } else {
+            A = A / B;
             return A;
         }
+    }
 
-        public static double getDifference(double A, double B) {
-            A = A - B;
-            return A;
-        }
+    public static String branchTest(String test){
+        test = "Branch test successful";
+        return test;
+    }
 
-        public static double getProduct(double A, double B) {
-            A = A * B;
-            return A;
-        }
-        
-        public static double getQuotient(double A, double B) {
-            if (B == 0) {
-                System.out.println("Error: Division by zero is not allowed.");
-                return A;
-            } else {
-                A = A / B;
-                return A;
-            }
-        }
 }
 
-
-
-
-
-
-
-    /*     
-            
-        // switch between menu options to select relevant code
-            switch (choice) {
-                case "a" -> {
-                    System.out.print("Enter a value for A: ");
-                    A = input.nextDouble();
-                    input.nextLine();
-                    System.out.println("A = " + A);
-                }
-                case "b" -> {
-                    System.out.print("Enter a value for B: ");
-                    B = input.nextDouble();
-                    input.nextLine();
-                    System.out.println("B = " + B);
-                }
-                case "+" -> {
-                    result = A + B;
-                    System.out.printf("Result: %.3f\n", result);
-                }
-                case "-" -> {
-                    result = A - B;
-                    System.out.printf("Result: %.3f\n", result);
-                }
-                case "*" -> {
-                    result = A * B;
-                    System.out.printf("Result: %.3f\n", result);
-                }
-                case "/" -> {
-                    if (B == 0) {
-                        System.out.println("Error: Division by zero is not allowed.");
-                    } else {
-                        result = A / B;
-                        System.out.printf("Result: %.3f\n", result);
-                    }
-                }
-                case "c" -> {
-                    A = 0.000;
-                    B = 0.000;
-                    result = 0.000;
-                    System.out.println("Cleared. A = " + A + "\t B = " + B);
-                }
-                case "q" -> System.out.println("System terminated.");
-                default -> System.out.println("Invalid entry. Please try again.");
-            }
-        }
-                }
-    } */
+/*
+ * 
+ * // switch between menu options to select relevant code
+ * switch (choice) {
+ * case "a" -> {
+ * System.out.print("Enter a value for A: ");
+ * A = input.nextDouble();
+ * input.nextLine();
+ * System.out.println("A = " + A);
+ * }
+ * case "b" -> {
+ * System.out.print("Enter a value for B: ");
+ * B = input.nextDouble();
+ * input.nextLine();
+ * System.out.println("B = " + B);
+ * }
+ * case "+" -> {
+ * result = A + B;
+ * System.out.printf("Result: %.3f\n", result);
+ * }
+ * case "-" -> {
+ * result = A - B;
+ * System.out.printf("Result: %.3f\n", result);
+ * }
+ * case "*" -> {
+ * result = A * B;
+ * System.out.printf("Result: %.3f\n", result);
+ * }
+ * case "/" -> {
+ * if (B == 0) {
+ * System.out.println("Error: Division by zero is not allowed.");
+ * } else {
+ * result = A / B;
+ * System.out.printf("Result: %.3f\n", result);
+ * }
+ * }
+ * case "c" -> {
+ * A = 0.000;
+ * B = 0.000;
+ * result = 0.000;
+ * System.out.println("Cleared. A = " + A + "\t B = " + B);
+ * }
+ * case "q" -> System.out.println("System terminated.");
+ * default -> System.out.println("Invalid entry. Please try again.");
+ * }
+ * }
+ * }
+ * }
+ */
