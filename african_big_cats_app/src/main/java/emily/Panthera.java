ea13910cce1098957d21b72c9213ad95d2a54908
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2025-09-28 17:45:25 -0700

    Initial Big Cats App commit; created Panthera subclass

---
+++ african_big_cats_app/src/main/java/emily/Panthera.java
@@ -0,0 +1,14 @@
+package emily;
+
+public class Panthera extends PantheraGPS {
+
+    private Object PantheraGPS;
+    private int weight;
+
+// constructor
+public Panthera(Object PantheraGPS, int weight) {
+    this.PantheraGPS = PantheraGPS;
+    this.weight = weight;
+}; 
+
+}
