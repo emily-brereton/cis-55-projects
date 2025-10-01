2b57882df2b27bd04b38ad4c2dd68812e83668d7
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2025-10-01 00:34:54 -0700

    Added "Menu" class, added methods for printing menu and obtaining user input

--- african_big_cats_app/src/main/java/emily/App.java
+++ african_big_cats_app/src/main/java/emily/App.java
@@ -1,11 +1,42 @@
 package emily;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
- * Hello world!
+ * Student Name: Emily Brereton
+ * Course: CIS 59-101
+ * Term/Year: Fall 2025
+ * Date: 10/23/25
+ * Project Name: African Big Cats App
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ArrayList<Panthera> allBigCats = new ArrayList<Panthera>();
+        Scanner input = new Scanner(System.in);
+        
+        new Menu().printMenu();
+        String selection= input.nextLine().toUpperCase();
+
     }
+}
+
+class Menu {
+
+    public void printMenu(){
+        System.out.println("""
+                ------------------------------------------------------------
+                African Big Cats App
+                ------------------------------------------------------------
+                    c       [C]reate a big cat
+                    d       [D]elete a big cat
+                    f       [F]ind a big cat
+                    l       [L]ist all big cats
+                    q       [Q]uit
+                ------------------------------------------------------------
+                Enter a command: 
+                """);
+    }
+
 }
