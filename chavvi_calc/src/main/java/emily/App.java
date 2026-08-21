90f6fb93020e2a06efa157550605226074dab500
Emily Brereton <86748330+emilyjb22@users.noreply.github.com>
2026-08-20 18:42:22 -0700

    reordered methods in Calc class, deleted duplicate getA/getB method

--- chavvi_calc/src/main/java/emily/App.java
+++ chavvi_calc/src/main/java/emily/App.java
@@ -6,9 +6,9 @@ import java.math.RoundingMode;
 
 /*
  * Student Name: Emily Brereton
- * Course: CIS 59-101
- * Term/Year: Fall 2025
- * Date: 9/25/25
+ * Course: CIS 55-101
+ * Term/Year: Fall 2026
+ * Date: 8/20/26
  * Project Name: ChavviCalc App
  */
 public class App {
@@ -30,10 +30,10 @@ public class App {
 
             switch (choice) {
                 case "a","A" -> {
-                    a = calc.getA(input);
+                    a = calc.saveX("Enter a value for a: ",input);
                 }
                 case "b","B" -> {
-                    b = calc.getB(input);
+                    b = calc.saveX("Enter a value for b: ",input);
                 }
                 case "+" -> {
                     a = calc.add(a,b);
@@ -48,8 +48,8 @@ public class App {
                     a = calc.divide(a,b);
                 }
                 case "c","C" -> {
-                    a = calc.clearA(a);
-                    b = calc.clearB(b);
+                    a = calc.clear(a);
+                    b = calc.clear(b);
                 }
                 case "q","Q" -> System.out.println("System terminated.");
                 default -> System.out.println("Invalid entry. Please try again.");
@@ -77,9 +77,18 @@ class Calc {
         this.b = b;
     }
 
+
+    public void setA(float a) {
+        this.a = a;
+    }
+
+    public void setB(float b) {
+        this.b = b;
+    }
+    
     // method to obtain a value for any variable
     // try/catch statement: try prompts user to enter data; catch prevents exception when user enters non-numerical data
-    public float getNumber(Scanner input,float n) {
+    private float getNumber(Scanner input,float n) {
         try {
             n = input.nextFloat();
             input.nextLine();
@@ -92,42 +101,29 @@ class Calc {
         }
     }
 
-    public void setA(float a) {
-        this.a = a;
-    }
-    
-    public float getA(Scanner input) {
-        System.out.println("Enter a value for a: ");
-        a = getNumber(input,a);
-        return a;
+    public float saveX(String message, Scanner input) {
+        System.out.println(message);
+        return getNumber(input,a);
     }
 
-    public void setB(float b) {
-        this.b = b;
-    }
 
-    public float getB(Scanner input) {
-        System.out.println("Enter a value for b: ");
-        b = getNumber(input, b);
-        return b;
-    }
+    // public float getB(Scanner input) {
+    //     System.out.println("Enter a value for b: ");
+    //     return getNumber(input, b);
+    // }
 
-    // adds a and b, sum is saved in variable a
     public float add(float a,float b) {
         return a += b;
     }
 
-    // subtracts b from a, difference is saved in variable a 
     public float subtract(float a,float b) {
         return a -= b;
     }
 
-    // multiplies a and b, product is saved in variable a
     public float multiply(float a,float b) {
         return a *= b;
     }
 
-    // divides a by b, quotient is saved in variable a
     // if b = 0, error message is printed and a remains unchanged
     public float divide(float a,float b) {
         if (b == 0) {
@@ -138,16 +134,9 @@ class Calc {
         }
     }
 
-    // clears the value of a, returns a as 0.000
-    public float clearA(float a) {
-        a = 0.0f;
+    // reset variable to 0.0
+    public float clear(float x) {
         return 0.0f;
-    }
-
-    // clears the value of b, returns b as 0.000
-    public float clearB(float b) {
-        b = 0.0f;
-        return b;
     }
 
     // prints the menu to the console, formats and rounds a and b to three places 
